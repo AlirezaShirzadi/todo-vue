5db6f3bbb7ca160576eb6302f7cbad0b256ee1d5
Alireza Shirzadi <shirzadiwork@gmail.com>
2024-11-04 11:22:26 +0330

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref, computed } from 'vue'
+
+const itemList = ref({
+  todo: [],
+  inProgress: [],
+  done: [],
+})
+
+const todoLength = computed(() => {
+  return itemList.value.todo.length
+})
+const inProgressLength = computed(() => {
+  return itemList.value.inProgress.length
+})
+const doneLength = computed(() => {
+  return itemList.value.done.length
+})
+</script>
+
+<template>
+  <main>
+    <div class="lengthWrapper">
+      <div>Todo: {{ todoLength }}</div>
+      <div>In Progress: {{ inProgressLength }}</div>
+      <div>Done: {{ doneLength }}</div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.lengthWrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  column-gap: 16px;
+}
+</style>
